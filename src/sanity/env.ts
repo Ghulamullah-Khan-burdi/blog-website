6c7a8886061f394551cec1c73d391d45ae3f41e5
Ghulamullah-Khan-burdi <ghulamullahkhan2005@gmail.com>
2025-02-10 13:46:42 +0500

    first commit

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -2,13 +2,12 @@ export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-01-30'
 
 export const dataset = 
-  process.env.NEXT_PUBLIC_SANITY_DATASET || 'product'
+  process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
   
 
-export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
-)
+export const projectId = 
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "7by7bl0w"
+ 
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
